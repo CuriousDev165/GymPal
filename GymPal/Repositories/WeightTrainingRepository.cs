85f4d5581f5b998be35f5f24771605453bb22466
Micah Smith <111553196+CuriousDev165@users.noreply.github.com>
2025-07-16 18:14:55 -0500

    Added WeightTrainingRepository for weight training records.

---
+++ GymPal/Repositories/WeightTrainingRepository.cs
@@ -0,0 +1,33 @@
+﻿using GymPal.Interfaces;
+
+namespace GymPal.Repositories
+{
+    // Repository for working with weight training records in a database.
+    public class WeightTrainingRepository : IRepository
+    {
+        public async Task<int> AddRecordAsync<WeightTrainingMovement>(WeightTrainingMovement record) where WeightTrainingMovement : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> AddRecords<WeightTrainingMovement>(IEnumerable<WeightTrainingMovement> records) where WeightTrainingMovement : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> DeleteRecordAsync<WeightTrainingMovement>(WeightTrainingMovement argument) where WeightTrainingMovement : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<WeightTrainingMovement> GetRecordAsync<WeightTrainingMovement>(WeightTrainingMovement argument) where WeightTrainingMovement : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<WeightTrainingMovement>> GetRecordsAsync<WeightTrainingMovement>(WeightTrainingMovement argument) where WeightTrainingMovement : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
