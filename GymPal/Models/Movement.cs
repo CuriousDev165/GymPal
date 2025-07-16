e78b02b7f5e4a7fd6475074654d518117cc8d76e
Micah Smith <111553196+CuriousDev165@users.noreply.github.com>
2025-07-16 17:41:59 -0500

    Added the Movement and WeightTrainingMovement classes.

---
+++ GymPal/Models/Movement.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace GymPal.Models
+{
+    // Base class for all movement types. Only use this class to define shared movement properties.
+    // Any public property defined here will be included as a table column in inherited classes that use SQLite.
+    public class Movement
+    {
+        [PrimaryKey, Unique, AutoIncrement]
+        public int Id { get; set; }
+    }
+}
---
+++ GymPal/Models/WeightTrainingMovement.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace GymPal.Models
+{
+    public class WeightTrainingMovement : Movement
+    {
+        [NotNull, MaxLength(40)]
+        public required string Name { get; set; }
+        public int SetNumber { get; set; }
+        public double Weight { get; set; }
+        public int Reps { get; set; }
+    }
+}
