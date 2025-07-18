43a491e76ec0f8d4eb83511c32708f47fdcb3e8f
Micah Smith <111553196+CuriousDev165@users.noreply.github.com>
2025-07-18 13:39:40 -0500

    Added viewmodel members and methods with associated unit tests.

--- GymPal.Core/ViewModels/MainPageViewModel.cs
+++ GymPal.Core/ViewModels/MainPageViewModel.cs
@@ -1,14 +1,84 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GymPal.Interfaces;
+using GymPal.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GymPal.ViewModels
 {
-    public partial class MainPageViewModel : ObservableObject
+    public partial class MainPageViewModel(IRepository _repo) : ObservableObject
     {
+        private readonly IRepository repo = _repo;
 
+        [ObservableProperty]
+        public partial string NewMovement { get; set; }
+
+        [ObservableProperty]
+        public partial ObservableCollection<string> Movements { get; set; } = [];
+        [ObservableProperty]
+        public partial string CurrentMovement { get; set; }
+        // Defaults to the first set when the page loads.
+        [ObservableProperty]
+        public partial int SetNumber { get; set; } = 1;
+        [ObservableProperty]
+        public partial int Weight { get; set; }
+        [ObservableProperty]
+        public partial int Reps { get; set; }
+
+        // Add a new movement to the movement table in the database, refresh the movement list, and clear the new movement field.
+        [RelayCommand]
+        public async Task AddNewMovementAsync()
+        {
+            var rowsAffected = await repo.AddRecordAsync(new Movement { Name = NewMovement });
+            if(rowsAffected == 1)
+            {
+                // Refresh the movement list.
+                Movements.Clear();
+                await GetMovementsAsync();
+            }
+
+            NewMovement = string.Empty;
+        }
+
+        // Retrieve the list of movements from the database and populate the Movements collection.
+        [RelayCommand]
+        public async Task GetMovementsAsync()
+        {
+            var movements = await repo.GetRecordsAsync(new Movement { Name = string.Empty });
+            foreach(var movement in movements)
+            {
+                Movements.Add(movement.Name);
+            }
+        }
+
+        // Resets the set number, weight, and reps to their default values when either the current movement is changed or a set is saved.
+        [RelayCommand]
+        public void ResetSetValues()
+        {
+            SetNumber = 1;
+            Weight = 0;
+            Reps = 0;
+        }
+
+        // Save the current set to the database.
+        [RelayCommand]
+        public async Task SaveSetAsync()
+        {
+            await repo.AddRecordAsync(new WeightTrainingMovement
+            {
+                Name = CurrentMovement,
+                Date = DateOnly.FromDateTime(DateTime.Now).ToString("yyyy-MM-dd"),
+                SetNumber = SetNumber,
+                Weight = Weight,
+                Reps = Reps
+            });
+
+            ResetSetValues();
+        }
     }
 }
---
+++ GymPalUnitTests/ViewModelTests/MainPageViewModelTests.cs
@@ -0,0 +1,95 @@
+﻿using GymPal.Interfaces;
+using GymPal.Models;
+using GymPal.Repositories;
+using GymPal.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymPal.UnitTests.ViewModelTests
+{
+    public class MainPageViewModelTests
+    {
+        private readonly MainPageViewModel viewModel;
+        private readonly Mock<IRepository> mockRepo = new();
+
+        private List<string> Movements { get; set; } = [];
+
+        public MainPageViewModelTests()
+        {
+            viewModel = new (mockRepo.Object);
+        }
+
+        [Fact]
+        public async Task AddNewMovementAsync_RefreshesMovementList()
+        {
+            // Arrange
+            viewModel.NewMovement = "New Movement";
+            mockRepo.Setup(repo => repo.AddRecordAsync(It.IsAny<Movement>()))
+                .ReturnsAsync(1);
+            mockRepo.Setup(repo => repo.GetRecordsAsync(It.IsAny<Movement>()))
+                .ReturnsAsync([new() { Name = "New Movement" }]);
+
+            // Act
+            await viewModel.AddNewMovementAsync();
+
+            // Assert
+            Assert.Single(viewModel.Movements);
+            Assert.Equal("New Movement", viewModel.Movements[0]);
+            Assert.Empty(viewModel.NewMovement);
+        }
+
+        [Fact]
+        public async Task GetMovementsAsync_RefreshesMovementList()
+        {
+            // Arrange
+            mockRepo.Setup(repo => repo.GetRecordsAsync(It.IsAny<Movement>())).
+                ReturnsAsync([new() { Name = "Movement 1" }]);
+
+            // Act
+            await viewModel.GetMovementsAsync();
+
+            // Assert
+            Assert.Single(viewModel.Movements);
+            Assert.Equal("Movement 1", viewModel.Movements[0]);
+        }
+
+        [Fact]
+        public void ResetSetValues_ResetsSetValues()
+        {
+            // Arrange
+            viewModel.SetNumber = 2;
+            viewModel.Weight = 100;
+            viewModel.Reps = 10;
+
+            // Act
+            viewModel.ResetSetValues();
+
+            // Assert
+            Assert.Equal(1, viewModel.SetNumber);
+            Assert.Equal(0, viewModel.Weight);
+            Assert.Equal(0, viewModel.Reps);
+        }
+
+        [Fact]
+        public async Task SaveSetAsync_ResetsSetValues()
+        {
+            // Arrange
+            viewModel.SetNumber = 2;
+            viewModel.Weight = 100;
+            viewModel.Reps = 10;
+            mockRepo.Setup(repo => repo.AddRecordAsync(It.IsAny<WeightTrainingMovement>()))
+                .ReturnsAsync(1);
+            // Act
+            await viewModel.SaveSetAsync();
+
+            // Assert
+            Assert.Equal(1, viewModel.SetNumber);
+            Assert.Equal(0, viewModel.Weight);
+            Assert.Equal(0, viewModel.Reps);
+        }
+    }
+}
