6d2b4ebc1a27c257cb143f94f839592e3206c0db
Micah Smith <111553196+CuriousDev165@users.noreply.github.com>
2025-07-18 14:01:49 -0500

    Created MovementService to share the movement list between viewmodels.

---
+++ GymPal.Core/Services/MovementService.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using GymPal.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymPal.Core.Services
+{
+    public partial class MovementService : ObservableObject
+    {
+        [ObservableProperty]
+        public partial ObservableCollection<string> Movements { get; set; } = [];
+    }
+}
